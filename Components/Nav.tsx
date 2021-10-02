6235ce076b70c0a5631faa11314a9a398204c74a
Mohammed Amin Abdisamed <amiin@Mohammeds-MacBook-Pro.local>
2021-10-02 21:02:30 +0300

    bluecit website structure

---
+++ Components/Nav.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+
+
+const Nav = ()=>{
+
+    return (
+        <ul>
+            <li>
+                Home
+            </li>  
+            <li>
+                About
+            </li> 
+            <li>
+                Services
+            </li> 
+        </ul>
+    )
+}
+
+
+export default Nav
