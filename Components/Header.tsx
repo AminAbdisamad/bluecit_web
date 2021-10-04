f0fbb988b9fd784f8153df48b04cc3ee35dd821f
Mohammed Amin Abdisamed <amiin@Mohammeds-MacBook-Pro.local>
2021-10-05 00:43:53 +0300

    code linting fix

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,26 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-import Nav from './Nav'
+import React from "react";
+import styled from "styled-components";
+import Nav from "./Nav";
 
 const HeaderStyle = styled.div`
-display:flex;
-justify-content:space-between;
-align-content: center;
-padding: 0 10rem;
-`
-
-
-
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  padding: 0 10rem;
+`;
 
 function Header() {
-    return (
-        <HeaderStyle>
-            <h1>
-                BlueCIT
-            </h1>
-            <Nav/>
-        </HeaderStyle>
-    )
+  return (
+    <HeaderStyle>
+      <h1>BlueCIT</h1>
+      <Nav />
+    </HeaderStyle>
+  );
 }
 
-export default Header
+export default Header;
